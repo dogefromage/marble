6be5d2dc472c37ff1ba3a56f5b46fccec33307e4
Sebastian Saner <seb.sa@eblcom.ch>
2023-02-04 16:22:31 +0100

    interpreter update

--- packages/interpreter/src/interpreter.spec.ts
+++ packages/interpreter/src/interpreter.spec.ts
@@ -5,14 +5,12 @@ const glsl = (strings: TemplateStringsArray, ...values: string[]) =>
     String.raw({ raw: strings }, ...values);
 
 describe('Interpreter', () => {
-
     test('Test', () => {
         const ast = parser.parse(glsl`
             int add(int a, int b) {
                 return a + b;
             }
         `);
-
         const interpreter = new Interpreter();
         interpreter.loadAst(ast);
     });
--- packages/interpreter/src/interpreter.ts
+++ packages/interpreter/src/interpreter.ts
@@ -1,22 +1,63 @@
-import { AstNode, Program } from "@shaderfrog/glsl-parser/ast";
+import { AstNode, BinaryNode, DeclarationNode, ExpressionStatementNode, IfStatementNode, Program } from "@shaderfrog/glsl-parser/ast";
+import fs from 'fs';
+import path from 'path';
 
-type Definition =
-    | { type: 'value', value: any }
-    | { type: 'function', value: any }
+type IterationStatementNode = never;
+type JumpStatementNode = never;
 
-type Scope = Map<string, Definition>;
+type SimpleStatementNode =
+    | DeclarationNode
+    | ExpressionStatementNode
+    | IfStatementNode
+    | IterationStatementNode
+    | JumpStatementNode
+
+class NodeNotFoundError extends Error {
+    constructor(nodeType: string) {
+        super(`Node type ${nodeType} not found.`);
+    }
+}
 
 export class Interpreter {
 
-    private scopes: Scope[] = [];
+    private astProgram!: Program;
 
     constructor() {}
 
     loadAst(astProgram: Program) {
-        console.log(astProgram);
+        this.astProgram = astProgram;
+
+        const json = JSON.stringify(astProgram, undefined, 4);
+        fs.writeFileSync(path.resolve('./ast.json'), json);
     }
 
-    interpret(expression: AstNode) {
+    public interpretSimpleStatement(node: SimpleStatementNode) {
+        switch (node.type) {
+            case 'declaration':
+                return this.interpretDeclaration(node);
+            case 'expression_statement':
+                return this.interpretExpressionStatement(node);
+            case 'if_statement':
+                return this.interpretIfStatement(node);
+            default:
+                throw new NodeNotFoundError((node as AstNode).type);
+        }
+    }
+
+    private interpretDeclaration(node: DeclarationNode) {
+        // https://docs.nvidia.com/drive/archive/5.1.0.2L/nvvib_docs/DRIVE_OS_Linux_SDK_Development_Guide/baggage/GLSL_ES_Specification_3.20.withchanges.pdf
+    }
+
+    private interpretExpressionStatement(node: ExpressionStatementNode) {
+
+    }
+
+    private interpretIfStatement(node: IfStatementNode) {
+
+    }
+
+
+    private interpretBinary(expression: BinaryNode) {
 
     }
 }
