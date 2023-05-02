a7472460fd9046f989e277c8bc5960f50d5aa29b
Sebastian Saner <seb.sa@eblcom.ch>
2023-05-02 21:31:23 +0200

    deploy test

--- build.sh
+++ build.sh
@@ -8,14 +8,34 @@ echo ">>>> Building language"
 cd $base_path/language
 yarn build
 
+echo ">>>> Linking Monorepo"
+cd $base_path
+yarn
+
 echo ">>>> Building vite-plugin-glsl-templates"
 cd $base_path/vite-plugin-glsl-templates
 yarn build
+
+echo ">>>> Linking Monorepo"
+cd $base_path
+yarn
 
 echo ">>>> Building app"
 cd $base_path/app
 yarn build
 
+echo ">>>> Linking Monorepo"
+cd $base_path
+yarn
+
+echo ">>>> Linking Monorepo"
+cd $base_path
+yarn
+
 echo ">>>> Building web"
 cd $base_path/web
 yarn build
+
+echo ">>>> Linking Monorepo"
+cd $base_path
+yarn
